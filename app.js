2404a72d38327c3d9d05af940db1fd2e76e1d94d
Ali Mahdavi <mahdavi095@gmail.com>
2017-08-25 14:25:25 +0430

    send simle variable to views>index.ejs

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.set('view engine', 'ejs')
 app.set('views' , path.join(__dirname,'views'))
 
 app.get('/',function(req, res){
-    res.render('index')
+    res.render('index',{
+        title:'Express'
+    }) 
 })
 
 
