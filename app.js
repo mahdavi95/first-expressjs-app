826fbc47d160673267adbe562af5ca797977014a
Ali Mahdavi <mahdavi095@gmail.com>
2017-08-25 14:17:53 +0430

    add views folder and index.ejs

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs')
 app.set('views' , path.join(__dirname,'views'))
 
 app.get('/',function(req, res){
-    res.send('Hello My Express App ;))')
+    res.render('index')
 })
 
 
